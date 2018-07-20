7745249beeeadf4de2a61fa901f908b7873893a6
Bob Hall <myk_quayce@hotmail.com>
2018-07-20 19:11:00 +0100

    Add project files.

---
+++ Cineworld.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cineworld.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
