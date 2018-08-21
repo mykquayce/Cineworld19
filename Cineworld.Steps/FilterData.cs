e2f7d3d7b92721db4afcb903e0262898817065bf
Bob Hall <Robert.Hall@carfinance247.co.uk>
2018-08-21 07:56:19 +0100

    Revert "tsrsrattsra"
    This reverts commit d54b9b34ce7dfd05208de54ced117288f5f514ba.

--- Cineworld.Steps/FilterData.cs
+++ Cineworld.Steps/FilterData.cs
@@ -9,22 +9,75 @@ namespace Cineworld.Steps
 {
 	public class FilterData : StepBody
 	{
-        private readonly Services.IFilterService _filterService;
+		private readonly Services.ISerializationService _serializationService;
 
 		public FilterData(
-			Services.IFilterService filterService)
+			Services.ISerializationService serializationService)
 		{
-            _filterService = filterService;
+			_serializationService = serializationService;
 		}
 
 		public cinemasType Original { get; set; }
 		public cinemasType Filtered { get; set; }
+		public IEnumerable<Predicate<cinemaType>> CinemaFilters { get; set; }
+		public IEnumerable<Predicate<filmType>> FilmFilters { get; set; }
+		public IEnumerable<Predicate<showType>> ShowFilters { get; set; }
 
 		public override ExecutionResult Run(IStepExecutionContext context)
 		{
-            Filtered = new cinemasType { cinema = _filterService.Filter(Original.cinema), };
+			var cinemas = new List<cinemaType>();
+
+			foreach (var cinema in FIlterCinemas(Original.cinema))
+			{
+				var films = new List<filmType>();
+
+				foreach (var film in FilterFilms(cinema.listing))
+				{
+					var filtered = FIlterShows(film.shows).ToArray();
+					var cloned = _serializationService.DeepClone(filtered);
+
+					if (cloned.Any())
+					{
+						var f = _serializationService.DeepClone(film);
+
+						f.shows = cloned;
+
+						films.Add(f);
+					}
+				}
+
+				if (films.Any())
+				{
+					var c = _serializationService.DeepClone(cinema);
+
+					c.listing = films.ToArray();
+
+					cinemas.Add(c);
+				}
+			}
+
+			Filtered = new cinemasType
+			{
+				cinema = cinemas.ToArray(),
+			};
 
 			return ExecutionResult.Next();
+		}
+
+		private IEnumerable<cinemaType> FIlterCinemas(IEnumerable<cinemaType> cinemas) => Filter(cinemas, CinemaFilters);
+		private IEnumerable<filmType> FilterFilms(IEnumerable<filmType> films) => Filter(films, FilmFilters);
+		private IEnumerable<showType> FIlterShows(IEnumerable<showType> shows) => Filter(shows, ShowFilters);
+
+		private static IEnumerable<T> Filter<T>(IEnumerable<T> items, IEnumerable<Predicate<T>> filters)
+		{
+			if (filters?.Any() ?? false)
+			{
+				return from i in items
+					   where filters.All(f => f(i))
+					   select i;
+			}
+
+			return items;
 		}
 	}
 }
